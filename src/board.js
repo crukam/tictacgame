be3ed2526fcf02a3a9e6da08320cec11982a921c
Claude Rukamiza <jnkurunz0912@gmail.com>
2019-09-16 01:23:10 +0100

    my working game with winner

--- src/board.js
+++ src/board.js
@@ -9,6 +9,7 @@ class Grid extends React.Component{
 	}
 	handleclick(i){
 		let square_values=this.state.square_values.slice();
+		if(square_values[i]||this.checkWinner(square_values)) return;
 		square_values[i]= this.state.x_index?'X':'O';
 		this.setState({square_values:square_values, x_index:!this.state.x_index});
 	
@@ -26,9 +27,31 @@ class Grid extends React.Component{
 		}
 		return(Grid);
     }
+	checkWinner(squares){
+	let lines=[[0,1,2],
+	           [3,4,5],
+			   [6,7,8],
+			   [1,4,7],
+			   [2,5,8],
+			   [0,3,6],
+			   [0,4,8],
+			   [2,4,6]
+			  ];
+    for( let i=0;i<lines.length;i++){
+	    let [a,b,c]=lines[i];
+		if(squares[a]&& squares[a]===squares[b] && squares[a]===squares[c])
+		{
+			return squares[a];
+		}
+	 }
+	 return null;
+	}
 	render(){
+	    let message= (this.checkWinner(this.state.square_values))? ('winner:' + this.checkWinner(this.state.square_values)):('Next player:' + (this.state.x_index ? 'X':'O'));
 		return(
+		    
 		     <div>
+			   <div className="message">{message}</div>
 		       {this.createGrid()}
 			</div>
 		);
